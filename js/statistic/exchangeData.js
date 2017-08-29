0c59b697c82fdf2f3c42eee106a1c496c8b429d6
sherllyLXL <511987975@qq.com>
2017-08-29 17:34:19 +0800

    兑换数据

---
+++ js/statistic/exchangeData.js
@@ -0,0 +1,12 @@
+//==============弹出层关闭
+$('#close_lay').click(function(){
+    $('#layer').fadeOut();
+});
+$('.save').click(function(){
+    $('#layer').fadeOut();
+});
+
+$('#add').click(function(){
+    $('#layer').fadeIn();
+});
+
